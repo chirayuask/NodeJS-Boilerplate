9ad0b81d64c9c95a5d5f470c2460c3a6675c1980
chirayuask <chirayuvarshney29@gmail.com>
2024-02-27 22:10:19 +0530

    Update rateLimiter.js

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 import env from '../config/env.js';
 
 export const rateLimiterUsingThirdParty = rateLimit({
-    windowMs: env.RATE_LIMITER_WINDOW_ALLOWED, // 24 hrs in milliseconds
+    windowMs: env.RATE_LIMITER_WINDOW_ALLOWED, 
     max: env.RATE_LIMITER_MAX_ALLOWED,
     message: 'You have exceeded the 100 requests in 24 hrs limit!',
     standardHeaders: true,
